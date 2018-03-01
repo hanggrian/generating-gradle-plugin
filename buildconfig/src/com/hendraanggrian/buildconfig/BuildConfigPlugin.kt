bcce7ec8e4950899f8d8a0dcd698e1dbd9b663a6
Hendra Anggrian <hendraanggrian@gmail.com>
2018-03-02 05:27:58 +0700

    Fix default values not applied

--- buildconfig/src/com/hendraanggrian/buildconfig/BuildConfigPlugin.kt
+++ buildconfig/src/com/hendraanggrian/buildconfig/BuildConfigPlugin.kt
@@ -11,30 +11,39 @@ import org.gradle.plugins.ide.idea.model.IdeaModel
 class BuildConfigPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
-        project.tasks {
-            val generateTask = "generate$CLASS_NAME"(BuildConfigTask::class) {
-                group = GROUP_NAME
+        project.run {
+            tasks {
+                val generateTask = "generate$CLASS_NAME"(BuildConfigTask::class) {
+                    group = GROUP_NAME
+                    afterEvaluate {
+                        if (packageName == null) packageName = project.group.toString()
+                        if (appName == null) appName = project.name.toString()
+                        if (groupId == null) groupId = project.group.toString()
+                        if (isVersionNull()) version(project.version.toString())
+                        if (debug == null) debug = false
+                    }
+                }
+
+                val compileTask = "compile$CLASS_NAME"(JavaCompile::class) {
+                    dependsOn(generateTask)
+                    group = GROUP_NAME
+                    classpath = files()
+                    destinationDir = buildDir.resolve("$GENERATED_DIRECTORY/buildconfig/classes/main")
+                    source(generateTask.outputDir)
+                }
+
+                val compiledClasses = files(compileTask.outputs.files.filter { !it.name.endsWith("dependency-cache") })
+                compiledClasses.builtBy(compileTask)
+
+                val sourceSet = convention.getPlugin(JavaPluginConvention::class.java).sourceSets["main"]
+                sourceSet.compileClasspath += compiledClasses
+                compiledClasses.forEach { sourceSet.output.dir(it) }
+
+                require(plugins.hasPlugin("org.gradle.idea")) { "plugin 'idea' must be applied" }
+                val providedConfig = configurations.create("provided$CLASS_NAME")
+                providedConfig.dependencies += dependencies.create(compiledClasses)
+                (extensions["idea"] as IdeaModel).module.scopes["PROVIDED"]!!["plus"]!! += providedConfig
             }
-
-            val compileTask = "compile$CLASS_NAME"(JavaCompile::class) {
-                dependsOn(generateTask)
-                group = GROUP_NAME
-                classpath = project.files()
-                destinationDir = project.buildDir.resolve("$GENERATED_DIRECTORY/buildconfig/classes/main")
-                source(generateTask.outputDir)
-            }
-
-            val compiledClasses = project.files(compileTask.outputs.files.filter { !it.name.endsWith("dependency-cache") })
-            compiledClasses.builtBy(compileTask)
-
-            val sourceSet = project.convention.getPlugin(JavaPluginConvention::class.java).sourceSets["main"]
-            sourceSet.compileClasspath += compiledClasses
-            compiledClasses.forEach { sourceSet.output.dir(it) }
-
-            require(project.plugins.hasPlugin("org.gradle.idea")) { "plugin 'idea' must be applied" }
-            val providedConfig = project.configurations.create("provided$CLASS_NAME")
-            providedConfig.dependencies += project.dependencies.create(compiledClasses)
-            (project.extensions["idea"] as IdeaModel).module.scopes["PROVIDED"]!!["plus"]!! += providedConfig
         }
     }
 
--- buildconfig/src/com/hendraanggrian/buildconfig/BuildConfigTask.kt
+++ buildconfig/src/com/hendraanggrian/buildconfig/BuildConfigTask.kt
@@ -9,6 +9,7 @@ import com.squareup.javapoet.TypeSpec
 import com.squareup.javapoet.TypeSpec.classBuilder
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import java.io.File
@@ -27,31 +28,31 @@ open class BuildConfigTask : DefaultTask() {
      * Package name of which `buildconfig` class will be generated to.
      * Default is project group.
      */
-    @Input var packageName: String = project.group.toString()
+    @Input var packageName: String? = null
 
     /**
      * Customize `BuildConfig.APP_NAME` value.
      * Default is project name.
      */
-    @Input var appName: String = project.name.toString()
+    @Input var appName: String? = null
 
     /**
      * Customize `BuildConfig.GROUP_ID` value.
      * Default is project group.
      */
-    @Input var groupId: String = project.group.toString()
+    @Input var groupId: String? = null
 
     /**
      * Customize `BuildConfig.VERSION` value.
      * Default is project version.
      */
-    @Input var version: String = project.version.toString()
+    @Input var version: String? = null
 
     /**
      * Customize `BuildConfig.DEBUG` value.
      * Default is false.
      */
-    @Input var debug: Boolean = false
+    @Input var debug: Boolean? = null
 
     @Input var fields: MutableMap<String, Pair<Class<*>, Any>> = mutableMapOf()
 
@@ -65,10 +66,10 @@ open class BuildConfigTask : DefaultTask() {
             .addModifiers(PUBLIC, FINAL)
             .addMethod(constructorBuilder().addModifiers(PRIVATE).build())
             .apply {
-                add(String::class.java, APP_NAME, appName)
-                add(String::class.java, GROUP_ID, groupId)
-                add(String::class.java, VERSION, version)
-                add(Boolean::class.java, DEBUG, debug)
+                add(String::class.java, APP_NAME, appName!!)
+                add(String::class.java, GROUP_ID, groupId!!)
+                add(String::class.java, VERSION, version!!)
+                add(Boolean::class.java, DEBUG, debug!!)
                 fields.forEach { name, (type, value) -> add(type, name, value) }
             }
             .build())
@@ -87,6 +88,13 @@ open class BuildConfigTask : DefaultTask() {
         require(isName(name)) { "$name is not a valid java variable name" }
         require(name !in RESERVED_NAMES) { "$name is reserved" }
         fields[name] = type to value
+    }
+
+    @Internal
+    internal fun isVersionNull(): Boolean = version == null
+
+    internal fun version(version: String) {
+        this.version = version
     }
 
     private fun TypeSpec.Builder.add(type: Class<*>, name: String, value: Any): TypeSpec.Builder = addField(builder(type, name, PUBLIC, STATIC, FINAL)
